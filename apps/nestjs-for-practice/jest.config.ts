851da5a363f9aef435b131b177405b26f632bf0c
fedaykin <fremanfedaykin@gmail.com>
2024-09-28 12:14:43 +0900

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/RIlY5qf3Dv

---
+++ apps/nestjs-for-practice/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nestjs-for-practice',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nestjs-for-practice',
+};
