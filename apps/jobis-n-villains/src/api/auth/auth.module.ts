6bc73a698d666f3a9361d9d20fbb887f204c34f7
fedaykin <fremanfedaykin@gmail.com>
2024-09-29 21:40:26 +0900

    feat: jwt access token 샘플 코드 추가

--- apps/jobis-n-villains/src/api/auth/auth.module.ts
+++ apps/jobis-n-villains/src/api/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [JwtModule.register({
+    global: true,
+  })],
   controllers: [AuthController],
   providers: [AuthService],
 })
